5997930f028a2b92472bf7b2d1d208240b7c5eb7
Sikharin John <sikharin.wandee@gmail.com>
2018-09-26 20:10:46 +0700

    Fixe local mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://test:sikharin1@ds115353.mlab.com:15353/todomongo');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
 
 module.exports = {mongoose};

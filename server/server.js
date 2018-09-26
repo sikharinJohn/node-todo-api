dc575d5b90585f060301d4af0baab8aa5a8eba3a
Sikharin John <sikharin.wandee@gmail.com>
2018-09-26 16:54:11 +0700

    Fixe GET /todos/:id with return case

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('/todos/:id', (req,res)=>{
   }
     Todo.findById(id).then( (todo)=>{
       if(!todo){
-        res.status(404).send();
+        return res.status(404).send();
       }
       res.send({todo});
     }).catch( (e)=> {

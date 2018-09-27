56884372f7a19edef75f9b6d597f00df6b1ee7d6
Sikharin John <sikharin.wandee@gmail.com>
2018-09-27 14:19:38 +0700

    Test DELETE /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -106,3 +106,39 @@ describe('GET /todos/id', ()=>{
 
 
 });
+
+describe('DELETE /todos/:id', ()=>{
+  it('should remove a todo and return doc', (done)=>{
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) =>{
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end( (err,res) =>{
+        if(err){
+          return done(err);
+        }
+        Todo.findById(hexId).then( (todos)=>{
+          expect(todos).toNotExist();
+          done();
+        }).catch( (e)=> done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done)=>{
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done)=>{
+    request(app)
+      .delete(`/todos/1234`)
+      .expect(404)
+      .end(done);
+  });
+
+});
